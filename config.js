99e70410b30f28db18d160afe5177eea0c26a1fe
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-22 13:27:45 +0200

    link

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // Deployment server:
-// export const url = "https://foofest.glitch.me";
-export const url = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const url = "https://marmalade-utopian-feels.glitch.me/";
