a3d141fc1c8c4fd75685ea74237bbede7fb6d1aa
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-27 09:47:23 +0200

    added overlay

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["nme.com", "billboard.com", "6.soompi.io", "www.hallyusg.net", "assets.teenvogue.com", "graziasg.s3.ap-southeast-1.amazonaws.com", "uproxx.com", "static01.nyt.com", "www.nme.com", "www.billboard.com", "metro.co.uk", "soompi.com", "teenvogue.com", "www.teenvogue.com", "grazia.sg", "hallyusg.net", "krazemag.com", "thebiaslist.com", "images.squarespace-cdn.com", "i8.amplience.net", "imageio.forbes.com", "www.gannett-cdn.com", "www.rollingstone.com"],
+    domains: ["nme.com", "billboard.com", "6.soompi.io", "www.hallyusg.net", "assets.teenvogue.com", "graziasg.s3.ap-southeast-1.amazonaws.com", "uproxx.com", "static01.nyt.com", "www.nme.com", "www.billboard.com", "metro.co.uk", "soompi.com", "teenvogue.com", "www.teenvogue.com", "grazia.sg", "hallyusg.net", "krazemag.com", "thebiaslist.com", "images.squarespace-cdn.com", "i8.amplience.net", "imageio.forbes.com", "www.gannett-cdn.com", "www.rollingstone.com", "assets.teenvogue.com"],
   },
 };
 export default nextConfig;
--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -20,14 +20,14 @@ export default function ScheduleCard({ scene, ...props }) {
   return (
     <>
       <Link prefetch={false} className="" href={`${props.slug}`}>
-        <div className="card w-36 bg-base-100 image-full cursor-pointer text-center">
+        <div className="card w-36 bg-base-100 image-full cursor-pointer text-center relative ">
           <div>
             <figure className={`h-36  relative border-2 rounded-3xl ${borderColor}`}>
               <div>
-                <Image className="opacity-60 rounded-3xl" src={props.src} alt={props.artist} layout="fill" objectFit="cover" />
+                <Image className="block w-full h-auto rounded-3xl" src={props.src} alt={props.artist} layout="fill" objectFit="cover" />
               </div>
-              <div>
-                <p className="text-xl text-gray-950 p-0 opacity-100 text-wrap break-words  ">{props.artist}</p>
+              <div className="absolute top-0 left-0 w-full h-full bg-blue-950 bg-opacity-50 flex items-center justify-center rounded-3xl ">
+                <p className="text-xl text-white p-0 text-wrap break-words  ">{props.artist}</p>
               </div>
             </figure>
           </div>
@@ -37,16 +37,6 @@ export default function ScheduleCard({ scene, ...props }) {
             <p className="text-2xl font-black">{props.time}</p>
             <div></div>
           </div>
-          {/* Display logo credits if there are any */}
-          {props.logoCredits && (
-            <div className="absolute bottom-2 right-12 z-40" data-tip={props.logoCredits}>
-              <span title={props.logoCredits} className="italic bg-slate-300 ">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </span>
-            </div>
-          )}
         </div>
       </Link>
       {/* Showing a break at the specific nextTime slot */}
