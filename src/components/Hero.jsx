f1b797ef6892d814456246a5fc3d0d756f61f75b
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-29 21:58:30 +0200

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Hero() {
-  return <Image src="/./img/heroimg.jpeg" width={700} height={700} alt="logo" className="logo" />;
+  return (
+    <div>
+      <Image src="/./img/heroimg.jpeg" width={700} height={700} alt="logo" className="logo" />{" "}
+    </div>
+  );
 }
