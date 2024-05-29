5009fa080a5b29ae6909a8ce396be874aa7b0d02
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-29 06:13:39 +0200

    stylet camp (booking)

--- src/components/booking/camping/CampingArea.jsx
+++ src/components/booking/camping/CampingArea.jsx
@@ -22,9 +22,9 @@ export default function CampingArea(props) {
   }, [isClicked]);
 
   return (
-    <div ref={divRef} className={`font-normal flex justify-between w-full hover:text-gray-100 border rounded-lg mb-4 py-7 px-6 bg-opacity-20 hover:bg-opacity-60 hover:cursor-pointer transition-all ${isActive ? "bg-opacity-60 text-gray-50" : "text-gray-400"} ${isClicked ? "ani_scale" : ""} ${props.available === 0 ? "text-gray-600 bg-gray-800 border-gray-900" : "bg-gray-700 border-gray-800"}`} onClick={handleClick}>
-      <p>{props.campName}</p>
-      <p>
+    <div ref={divRef} className={`font-normal flex justify-between w-full border-2 border-lime-500 bg-indigo-900 rounded-lg mb-4 py-4 px-6 bg-opacity-20 hover:bg-opacity-60 hover:text-white hover:scale-105 hover:cursor-pointer transition-all  ${isActive ? "bg-opacity-100 text-white" : "text-white"} ${isClicked ? "ani_scale" : ""} ${props.available === 0 ? "text-gray-600 bg-gray-800 border-gray-900" : "bg-gray-700 border-gray-800"}`} onClick={handleClick}>
+      <p style={{ fontFamily: "Syncopate, sans-serif", fontWeight: 700 }}>{props.campName}</p>
+      <p style={{ fontFamily: "Syncopate, sans-serif", fontWeight: 200 }}>
         {props.available} / {props.spots}
       </p>
     </div>
