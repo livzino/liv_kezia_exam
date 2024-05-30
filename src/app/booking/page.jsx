4d36092ab9d1f13178c61a2f967d701555bba377
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-30 11:48:40 +0200

    almost done

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -1,61 +1,56 @@
-"use client";
-import { useState, useEffect } from "react";
+"use client"; // Bruger klientindstillingen for dette modul
+import { useState, useEffect } from "react"; // Importér useState og useEffect hooks fra React
 /* import { sendMail } from "@/utils/sendMail";
- */ import { Optional } from "../../components/booking/Optional";
-import Tickets from "../../components/booking/Tickets";
-import TicketHolders from "../../components/booking/TicketHolders";
-import Payment from "../../components/booking/Payment";
-import PaymentStatus from "../../components/booking/PaymentStatus";
-import OrderSummary from "../../components/booking/OrderSummary";
-import MobileOrderSummary from "../../components/booking/MobileOrderSummery";
-import BackAndContinueButtons from "../../components/booking/BackAndContinueButtons";
-import Camping from "@/components/booking/camping/Camping";
+ */ import { Optional } from "../../components/booking/Optional"; // Importér Optional-komponenten
+import Tickets from "../../components/booking/Tickets"; // Importér Tickets-komponenten
+import TicketHolders from "../../components/booking/TicketHolders"; // Importér TicketHolders-komponenten
+import Payment from "../../components/booking/Payment"; // Importér Payment-komponenten
+import PaymentStatus from "../../components/booking/PaymentStatus"; // Importér PaymentStatus-komponenten
+import OrderSummary from "../../components/booking/OrderSummary"; // Importér OrderSummary-komponenten
+import MobileOrderSummary from "../../components/booking/MobileOrderSummery"; // Importér MobileOrderSummary-komponenten
+import BackAndContinueButtons from "../../components/booking/BackAndContinueButtons"; // Importér BackAndContinueButtons-komponenten
+import Camping from "@/components/booking/camping/Camping"; // Importér Camping-komponenten
 /*
-import { supabase } from "@/utils/supabaseClient"; */
-import { url } from "/config";
+import { supabase } from "@/utils/supabaseClient"; */ // Importér supabase-klienten
+import { url } from "/config"; // Importér URL'en fra konfigurationsfilen
 
+// Funktionen Page repræsenterer bookingsiden
 function Page() {
   // States
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const [allChoices, setAllChoices] = useState({});
-
-  const [regularTickets, setRegularTickets] = useState(0);
-  const [vipTickets, setVipTickets] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0); // State for den aktuelle slide
+  const [allChoices, setAllChoices] = useState({}); // State for alle valg
+  const [regularTickets, setRegularTickets] = useState(0); // State for antallet af almindelige billetter
+  const [vipTickets, setVipTickets] = useState(0); // State for antallet af VIP-billetter
   const [ticketHolders, setTicketHolders] = useState({
+    // State for billetindehavere
     regular: new Array(regularTickets).fill(""),
     vip: new Array(vipTickets).fill(""),
   });
-  const [totalTickets, setTotalTickets] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalTickets, setTotalTickets] = useState(0); // State for det samlede antal billetter
+  const [totalPrice, setTotalPrice] = useState(0); // State for den samlede pris
+  const [spots, setSpots] = useState([]); // State for tilgængelige pladser
+  const [selectedSpot, setSelectedSpot] = useState(null); // State for den valgte plads
+  const [selectedCamp, setSelectedCamp] = useState(null); // State for det valgte lejrområde
+  const [twoPersonTents, setTwoPersonTents] = useState(0); // State for antallet af telte til to personer
+  const [threePersonTents, setThreePersonTents] = useState(0); // State for antallet af telte til tre personer
+  const [greenCamping, setGreenCamping] = useState(false); // State for valget af grøn camping
+  const [totalSelectedCapacity, setTotalSelectedCapacity] = useState(0); // State for den samlede valgte kapacitet
+  const [email, setEmail] = useState(""); // State for e-mail
+  const [termsAccepted, setTermsAccepted] = useState(false); // State for accept af vilkår
+  const [reservationId, setReservationId] = useState(null); // State for reservations-ID
+  const [countdown, setCountdown] = useState(300); // State for nedtællingen
+  const [countdownInterval, setCountdownInterval] = useState(null); // State for intervallet for nedtællingen
+  const [minutes, setMinutes] = useState(5); // State for minutter
+  const [seconds, setSeconds] = useState(0); // State for sekunder
+  const [isPulsing, setIsPulsing] = useState(false); // State for puls-effekten
+  const [ticketsReserved, setTicketsReserved] = useState(false); // State for reserverede billetter
+  const [isModalOpen, setIsModalOpen] = useState(false); // State for åbning af modal
+  const [paymentSuccess, setPaymentSuccess] = useState(false); // State for betalingsstatus
+  const [warningCamp, setWarningCamp] = useState(false); // State for advarslen om lejrplads
 
-  const [spots, setSpots] = useState([]);
-  const [selectedSpot, setSelectedSpot] = useState(null);
-  const [selectedCamp, setSelectedCamp] = useState(null);
-
-  const [twoPersonTents, setTwoPersonTents] = useState(0);
-  const [threePersonTents, setThreePersonTents] = useState(0);
-  const [greenCamping, setGreenCamping] = useState(false);
-  const [totalSelectedCapacity, setTotalSelectedCapacity] = useState(0);
-
-  const [email, setEmail] = useState("");
-  const [termsAccepted, setTermsAccepted] = useState(false);
-
-  const [reservationId, setReservationId] = useState(null);
-
-  const [countdown, setCountdown] = useState(300);
-  const [countdownInterval, setCountdownInterval] = useState(null);
-  const [minutes, setMinutes] = useState(5);
-  const [seconds, setSeconds] = useState(0);
-  const [isPulsing, setIsPulsing] = useState(false);
-
-  const [ticketsReserved, setTicketsReserved] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [paymentSuccess, setPaymentSuccess] = useState(false);
-  const [warningCamp, setWarningCamp] = useState(false);
-
-  // Function to send mail with order confirmation to customer
+  // Funktion til afsendelse af e-mail med ordrebekræftelse til kunden
   function sendMailToCustomer() {
+    // Mailindhold
     const mailContent = {
       to: email,
       subject: "Order confirmation",
@@ -72,20 +67,20 @@ function Page() {
       sendername: "FooFest Customer Support",
       template: "foofest-template",
     };
-    sendMail(mailContent);
+    sendMail(mailContent); // Send mail med indholdet
   }
 
-  // Function to update the number of tickets
+  // Funktion til opdatering af antallet af billetter
   const updateTickets = (type, operation) => {
-    // Check if tickets are already reserved
+    // Tjek om billetter allerede er reserveret
     if (ticketsReserved) {
-      setIsModalOpen(true);
+      setIsModalOpen(true); // Åbn modal
       return;
     }
-    // Determine if the ticket type is VIP
+    // Bestem om billettypen er VIP
     const isVip = type === "vip";
     const currentTickets = isVip ? vipTickets : regularTickets;
-    // Check if operation is valid
+    // Tjek om operationen er gyldig
     if (operation === "increase" || (operation === "decrease" && currentTickets > 0)) {
       const newTickets = operation === "increase" ? currentTickets + 1 : currentTickets - 1;
       const setTickets = isVip ? setVipTickets : setRegularTickets;
@@ -93,11 +88,12 @@ function Page() {
     }
   };
 
-  // Function to update the number of tents
+  // Funktion til opdatering af antallet af telte
   const updateTents = (tentType, operation) => {
     const tentCapacity = tentType === "two" ? 2 : 3;
     const currentTents = tentType === "two" ? twoPersonTents : threePersonTents;
-    // Check if operation is valid
+
+    // Tjek om operationen er gyldig
     if (operation === "increase" && totalSelectedCapacity + tentCapacity <= totalTickets) {
       const newTents = currentTents + 1;
       const setTents = tentType === "two" ? setTwoPersonTents : setThreePersonTents;
@@ -111,7 +107,7 @@ function Page() {
     }
   };
 
-  // Function to change the slide
+  // Funktion til at skifte slide
   function changeSlide(direction) {
     if (direction === "next") {
       setCurrentSlide(currentSlide + 1);
@@ -123,11 +119,11 @@ function Page() {
   function handleContinue() {
     if (currentSlide === 1) {
       if (selectedCamp) {
-        // Continue only if a camp is selected
+        // Fortsæt kun hvis der er valgt en lejrplads
         changeSlide("next");
         reserveSpot();
       } else {
-        // Show an error message or prevent user from proceeding if no camp is selected
+        // Vis en fejlmeddelelse eller forhindr brugeren i at fortsætte, hvis ingen lejrplads er valgt
         alert("Please select a camp before proceeding.");
       }
     } else {
@@ -135,7 +131,7 @@ function Page() {
     }
   }
 
-  // Function to handle the modal confirm button
+  // Funktion til at håndtere bekræftelsesknappen i modalen
   function handleModalConfirm() {
     resetCountdown();
     setIsModalOpen(false);
@@ -143,7 +139,7 @@ function Page() {
     setReservationId(null);
   }
 
-  // Function to open the modal if tickets are reserved and user tries to change order
+  // Funktion til at åbne modalen hvis der er reserverede billetter og brugeren forsøger at ændre ordren
   function mapHandleModal() {
     if (ticketsReserved === true) {
       setIsModalOpen(true);
@@ -152,17 +148,17 @@ function Page() {
     }
   }
 
-  // Function to select a spot
+  // Funktion til valg af en plads
   function selectSpot(spot) {
     if (ticketsReserved === true) {
       setIsModalOpen(true);
     } else {
-      setSelectedSpot(spot.area); // Update selectedSpot with the area of the spot
-      setSelectedCamp(spot.area); // Update selectedCamp with the area of the spot
+      setSelectedSpot(spot.area); // Opdater valgteSpot med området for pladsen
+      setSelectedCamp(spot.area); // Opdater valgteCamp med området for pladsen
     }
   }
 
-  // Function to reserve a spot
+  // Funktion til reservation af en plads
   function reserveSpot() {
     if (ticketsReserved) {
       return;
@@ -212,7 +208,7 @@ function Page() {
       });
   }
 
-  // Function to send data to Supabase on successful payment
+  // Funktion til afsendelse af data til Supabase ved succesfuld betaling
   async function dataToSupabase() {
     const { data, error } = await supabase.from("orders").insert([
       {
@@ -235,7 +231,7 @@ function Page() {
     }
   }
 
-  // Function to fulfill the reservation using the API and the reservation ID
+  // Funktion til opfyldelse af reservationen ved hjælp af API'en og reservations-ID'et
   function fulfillReservation() {
     fetch(`${url}/fullfill-reservation`, {
       method: "POST",
@@ -254,7 +250,7 @@ function Page() {
       });
   }
 
-  // Reset the countdown and other states
+  // Nulstil nedtællingen og andre states
   function resetCountdown() {
     setCountdown(300);
     setMinutes(5);
@@ -266,30 +262,30 @@ function Page() {
     setTicketHolders({ regular: [], vip: [] });
   }
 
-  // Hook to check if the selected spot can accommodate the total tickets.
-  // If not, it sets a warning and resets the selected spot and camp.
-  // If the total tickets are zero or more than the available spots, it resets the selected spot and camp.
-  // If the selected spot can accommodate the total tickets, it removes the warning.
+  // Effekt til at kontrollere om den valgte plads kan rumme det samlede antal billetter.
+  // Hvis ikke, indstil en advarsel og nulstil den valgte plads og lejren.
+  // Hvis det samlede antal billetter er nul eller større end de tilgængelige pladser, nulstil den valgte plads og lejren.
+  // Hvis den valgte plads kan rumme det samlede antal billetter, fjern advarslen.
   useEffect(() => {
     const selectedSpotDetails = spots.find((spot) => spot.area === selectedSpot);
 
-    // Check if the selected spot can accommodate the total tickets
+    // Kontroller om den valgte plads kan rumme det samlede antal billetter
     if (selectedSpotDetails && totalTickets > selectedSpotDetails.available) {
       setWarningCamp(true);
       setSelectedSpot(null);
       setSelectedCamp(null);
     } else if (
-      // Check if the total tickets are zero or more than the available spots
+      // Kontroller om det samlede antal billetter er nul eller større end de tilgængelige pladser
       (selectedSpotDetails && totalTickets > selectedSpotDetails.available) ||
       totalTickets === 0
     ) {
-      // If no reservation ID, reset the selected spot and camp
+      // Hvis ingen reservations-ID, nulstil den valgte plads og lejren
       if (!reservationId) {
         setSelectedSpot(null);
         setSelectedCamp(null);
       }
     } else if (
-      // If the selected spot can accommodate the total tickets, remove the warning
+      // Hvis den valgte plads kan rumme det samlede antal billetter, fjern advarslen
       selectedSpotDetails &&
       totalTickets <= selectedSpotDetails.available
     ) {
@@ -297,7 +293,7 @@ function Page() {
     }
   }, [totalTickets, selectedSpot, spots, reservationId]);
 
-  // Calculate the total price and number of tickets, and update the allChoices object
+  // Beregn den samlede pris og antallet af billetter, og opdater allChoices-objektet
   useEffect(() => {
     const bookingFee = 99;
     const ticketPrice = regularTickets * 799 + vipTickets * 1299;
@@ -321,9 +317,9 @@ function Page() {
       reservationId,
       email,
     });
-  }, [regularTickets, vipTickets, selectedSpot, greenCamping, totalTickets, twoPersonTents, threePersonTents, ticketHolders, reservationId, email, ,]);
+  }, [regularTickets, vipTickets, selectedSpot, greenCamping, totalTickets, twoPersonTents, threePersonTents, ticketHolders, reservationId, email]);
 
-  // Fetch the available spots from the API on page load
+  // Hent de tilgængelige pladser fra API'en ved siden indlæses
   useEffect(() => {
     const fetchSpots = () => {
       fetch(`${url}/available-spots`)
@@ -339,24 +335,30 @@ function Page() {
 
   return (
     <main className="md:container mx-auto flex flex-col justify-center items-center h-screen w-screen">
+      {/* Modalen vises kun hvis isModalOpen er sand */}
       <dialog id="my_modal_1" className={isModalOpen ? "modal modal-open " : "modal"}>
         <div className="modal-box bg-gray-800 border border-gray-700 rounded-lg">
-          <h3 className="font-bold text-lg">Warning!</h3>
+          {/* Titlen og advarselsteksten i modalen */}
+          <h3 className="font-bold text-lg">Advarsel!</h3>
           <p className="py-4">
-            Changing your order will reset your reservation. <br></br>Are you sure you want to continue?
+            Ændring af din ordre vil nulstille din reservation. <br></br>Er du sikker på, at du vil fortsætte?
           </p>
           <div className="modal-action font-medium">
+            {/* Annuller-knappen, der lukker modalen uden at fortsætte */}
             <button className="btn btn-neutral font-medium text-base rounded py-1 px-4 w-fit" onClick={() => setIsModalOpen(false)}>
-              Cancel
+              Annuller
             </button>
+            {/* Bekræft-knappen, der lukker modalen og fortsætter bookingprocessen */}
             <button className="btn btn-primary font-medium text-emerald-100 text-base rounded py-1 px-4 w-fit border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400" onClick={handleModalConfirm}>
-              Confirm
+              Bekræft
             </button>
           </div>
         </div>
       </dialog>
 
+      {/* Sektionen indeholder de forskellige trin i bookingprocessen */}
       <section className="w-full h-full md:h-5/6 bg-violet-800 bg-opacity-50 max-w-7xl flex flex-col md:flex-row md:rounded-xl  border-rose-500 border-4  relative overflow-hidden">
+        {/* Indholdet afhænger af det aktuelle trin i bookingprocessen */}
         <div className={` ${ticketsReserved ? "mt-28" : "mt-12"} md:mt-0 bg-violet-800 bg-opacity-50 w-full  h-full order-2 md:order-1 p-6 md:p-12 flex flex-col justify-between`}>
           {(currentSlide === 0 && <Tickets regularTickets={regularTickets} vipTickets={vipTickets} totalTickets={totalTickets} spots={spots} selectedSpot={selectedSpot} updateTickets={updateTickets} selectSpot={selectSpot} setSelectedSpot={setSelectedSpot} ticketsReserved={ticketsReserved} selectedCamp={selectedCamp} setSelectedCamp={setSelectedCamp} mapHandleModal={mapHandleModal} reservationId={reservationId} warningCamp={warningCamp} />) ||
             (currentSlide === 1 && <Camping selectSpot={selectSpot} setSelectedSpot={setSelectedSpot} ticketsReserved={ticketsReserved} setSelectedCamp={setSelectedCamp} selectedCamp={selectedCamp} mapHandleModal={mapHandleModal} reservationId={reservationId} warningCamp={warningCamp} />) ||
@@ -364,6 +366,7 @@ function Page() {
             (currentSlide === 3 && <TicketHolders regularTickets={regularTickets} vipTickets={vipTickets} ticketHolders={ticketHolders} setTicketHolders={setTicketHolders} />) ||
             (currentSlide === 4 && <Payment email={email} setEmail={setEmail} termsAccepted={termsAccepted} setTermsAccepted={setTermsAccepted} />) ||
             (currentSlide === 5 && <PaymentStatus paymentSuccess={paymentSuccess} />)}
+          {/* Knapper til at gå tilbage og fortsætte i bookingprocessen */}
           <BackAndContinueButtons currentSlide={currentSlide} changeSlide={changeSlide} handleContinue={handleContinue} totalTickets={totalTickets} selectedSpot={selectedSpot} ticketHolders={ticketHolders} fulfillReservation={fulfillReservation} dataToSupabase={dataToSupabase} sendMailToCustomer={sendMailToCustomer} email={email} termsAccepted={termsAccepted} selectedCamp={selectedCamp} />
         </div>
         {currentSlide !== 5 && (
