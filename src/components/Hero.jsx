0b4e44c02a2ed2d9c0529602c036c3f0d9943075
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-30 12:01:38 +0200

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Hero() {
-  return <Image src="/./img/file.png" width={700} height={700} alt="logo" className="logo" />;
+  return <Image src="https://media.gq-magazine.co.uk/photos/638789fdf7c856d3214e7df6/16:9/w_5472,h_3078,c_limit/NO1_4461.jpg" width={700} height={700} alt="logo" className="logo" />;
 }
