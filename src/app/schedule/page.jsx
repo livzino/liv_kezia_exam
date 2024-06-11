4adbdfaafe5e8727be1baf71f240d4a0d0ef4ca8
Liv <livbendix@live.dk>
2024-06-11 13:02:11 +0200

    kjhgf

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -247,10 +247,12 @@ export default function Schedule() {
         {/* Her vises tidsplanen for hver ugedag */}
         <div className="flex flex-row lg:grid lg:grid-cols-7 lg:gap-4 overflow-x-scroll overflow-y-hidden snap-mandatory scrollbar-hide gap-x-6 scrollbar-hide mb-20 ring-4 ring-purple-950 ring-offset-4 ring-offset-slate-50 dark:ring-offset-cyan-300 rounded-3xl p-10 bg-fuchsia-950 opacity-90" style={{ fontFamily: "Syncopate, sans-serif", fontWeight: 400 }}>
           {/* Her mappes hen over hver dag og scene for at generere en tidsplan */}
-          {["mon", "tue", "wed", "thu", "fri", "sat", "sun"].map((dayName) => (
-            <div key={dayName} className="flex flex-col items-start">
-              <h2 className="text-xl font-bold mb-3 text-center">{dayNames[dayName].toUpperCase()}</h2>
-
+          {Object.keys(dataSchedule).map((scene) => {
+            const schedule = dataSchedule[scene][dayName];
+            <div key={scene} className="flex flex-col items-start">
+              <h2 className="text-xl font-bold mb-3 text-center">{selectedScene[scene].toUpperCase()</h2> 
+              })}
+        
               {/* Her mappes over hver handling udfra dag og scene */}
               {Object.keys(dataSchedule).map((scene) => {
                 const schedule = dataSchedule[scene][dayName];
@@ -281,8 +283,9 @@ export default function Schedule() {
                 });
               })}
             </div>
-          ))}
+          })}:
         </div>
+})}:
       </div>
     </>
   );
