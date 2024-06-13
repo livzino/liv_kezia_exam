8123e95d0899ff49731526d3c0ab743f806d9c2e
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-06-13 05:13:11 +0200

    map

--- src/components/CampMap.jsx
+++ src/components/CampMap.jsx
@@ -7,9 +7,9 @@ export default function Map() {
   return (
     // Definerer en sektion med klassen "map_container"
     <section className=" map_container flex flex-wrap justify-between items-center p-8 md:p-4 md:px-2 lg:p-3 lg:px-1 xl:p-2 xl:px-1 mb-4 bg-opacity-75 bg-pink-600 w-full">
-      <div className="flex-1 mx-4 md:mx-3 lg:mx-2 xl:mx-1 ">
+      <div className="flex-1 mx-4 md:mx-3 lg:mx-2 xl:mx-1  ">
         {/* Paragraf med beskrivelse af FooFest */}
-        <p className="text-lg md:text-base lg:text-xl xl:text-xl mb-4">Welcome to FooFest, the dream destination for K-pop lovers! Immerse yourself in a vibrant world filled with the beats and vibes of your favorite idols. Camp in areas themed after top groups like BTS, BLACKPINK, and TWICE, each offering its own unique atmosphere. Enjoy spectacular performances on stages inspired by legends like BIGBANG and SHINee. Celebrate, connect with fellow fans, and make unforgettable memories at FooFest, where K-pop comes to life in the most magical way!</p>
+        <p className="text-lg md:text-base lg:text-xl xl:text-xl mb-4 font-montserrat font-light">Welcome to FooFest, the dream destination for K-pop lovers! Immerse yourself in a vibrant world filled with the beats and vibes of your favorite idols. Camp in areas themed after top groups like BTS, BLACKPINK, and TWICE, each offering its own unique atmosphere. Enjoy spectacular performances on stages inspired by legends like BIGBANG and SHINee. Celebrate, connect with fellow fans, and make unforgettable memories at FooFest, where K-pop comes to life in the most magical way!</p>
         {/* Inkluderer en Button komponent med teksten "BOOK NOW" */}
         <Button text="BOOK NOW" />
       </div>
