f1f425ef6afbcdfecd30faafa5480c07a79bb4e4
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-06-13 11:54:26 +0200

    countdown

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -28,20 +28,20 @@ export default function CountDown({ targetDate }) {
 
   return (
     <div className="flex flex-row p-24 justify-center items-center bg-rose-600 h-full flex-grow">
-      <div className="text-lg font-bold text-center ">
-        <div className="bg-violet-950 place-items-center text-white rounded-md text-3xl p-2 ">{days}</div>
+      <div className="text-lg font-thin text-center md:text-1xl lg:text-4xl">
+        <div className="bg-violet-950 place-items-center text-white rounded-md p-2">{days}</div>
         <p>DAY</p>
       </div>
-      <div className="text-lg font-bold text-center mx-4">
-        <div className="bg-violet-950  place-items-center text-white rounded-md text-3xl p-2">{hours}</div>
+      <div className="text-lg font-thin text-center mx-4 md:text-1xl lg:text-4xl">
+        <div className="bg-violet-950  place-items-center text-white rounded-md p-2">{hours}</div>
         <p>HOUR</p>
       </div>
-      <div className="text-lg font-bold text-center mx-4">
-        <div className="bg-violet-950  place-items-center text-white rounded-md text-3xl p-2">{minutes}</div>
+      <div className="text-lg font-thin text-center mx-4 md:text-1xl lg:text-4xl">
+        <div className="bg-violet-950  place-items-center text-white rounded-md p-2">{minutes}</div>
         <p>MINUTE</p>
       </div>
-      <div className="text-lg font-bold text-center mx-4">
-        <div className="bg-violet-950 place-items-center text-white rounded-md text-3xl p-2">{seconds}</div>
+      <div className="text-lg font-thin text-center mx-4 md:text-1xl lg:text-4xl">
+        <div className="bg-violet-950 place-items-center text-white rounded-md p-2">{seconds}</div>
         <p>SECOND</p>
       </div>
     </div>
