cbe78a0a62d4dcc9629dd7bd1fe863401db32e58
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-06-13 05:15:10 +0200

    knap og kort

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default function Button(props) {
   return (
     <Link href="/booking">
-      <button className="text-m bg-gradient-to-r from-cyan-400 via-sky-500 to-blue-600  backdrop-blur-md transition-all hover:scale-105 border-4 border-lime-400 rounded-2xl text-lime-400 cursor-pointer h-14 px-12 max-w-s" style={{ fontFamily: "Syncopate, sans-serif", fontWeight: 700 }}>
-        {props.text}
-      </button>
+      <button className="text-m bg-gradient-to-r from-cyan-400 via-sky-500 to-blue-600  backdrop-blur-md transition-all hover:scale-105 border-4 border-lime-400 rounded-2xl text-lime-400 cursor-pointer h-14 px-12 max-w-s font-bold">{props.text}</button>
     </Link>
   );
 }
