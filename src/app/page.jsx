2ea555b8a53699b5d69cc5b29b9ced5eec3caded
Liv <livbendix@live.dk>
2024-06-14 12:20:02 +0200

    lkjhgv

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import Title from "../components/Title"; // Importér Title-komponenten
 import Ticket from "../components/Ticket"; // Importér Ticket-komponenten
 import Vip from "../components/Vip"; // Importér Vip-komponenten
 import Image from "next/image"; // Importér Image-komponenten fra Next.js
-
+import Link from "next/link";
 // Funktionen Home repræsenterer hovedsiden for webstedet
 export default function Home() {
   const targetDate = new Date("Jun 26, 2024").getTime(); // Definer måldatoen for nedtællingen
